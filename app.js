82ed49e2fad9e1a2dcf30a4346fef4e403e1795e
Matt Joslen <86894890+wildpineweb@users.noreply.github.com>
2021-10-14 12:39:32 -0700

    Removed unused JS code

--- app.js
+++ app.js
@@ -56,36 +56,3 @@ var $sideNavA = $(".side-nav a");
 var $isActiveHamburger = $(".hamburger.is-active .hamburger-inner::after");
 var $hamburgerInner = $(".hamburger-inner");
 var $mobileNav = $(".mobile-nav");
-
-/* Applicant SSN automated dashes in Application Form */
-const ssn = document.getElementById('ssn');
-
-ssn.addEventListener("keydown", (e) => {
-    if(e.key === "Backspace" || e.key === "Delete") return;
-    if(e.target.value.length === 3) {
-        ssn.value = ssn.value + "-";
-    }
-    if(e.target.value.length === 6) {
-        ssn.value = ssn.value + "-";
-    }
-});
-
-/* Co-Applicant SSN automated dashes in Application Form */
-const coSSN = document.getElementById('co-ssn');
-
-coSSN.addEventListener("keydown", (e) => {
-    if(e.key === "Backspace" || e.key === "Delete") return;
-    if(e.target.value.length === 3) {
-        coSSN.value = coSSN.value + "-";
-    }
-    if(e.target.value.length === 6) {
-        coSSN.value = coSSN.value + "-";
-    }
-});
-
-/* SSN Number Only Requirement */
-function isNumber(e){
-    e = e || window.event;
-    var charCode = e.which ? e.which : e.keyCode;
-    return /\d/.test(String.fromCharCode(charCode));
-}
